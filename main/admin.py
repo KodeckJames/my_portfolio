007e0bd0d0990a5d44bb188d818b92d9e6cfc719
Kodeck James <kodeckjames095@gmail.com>
2024-07-10 23:35:02 +0300

    Third commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Tag, Project, ProjectImage
 # Register your models here.
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "link")
+    inlines = [ProjectImageInline]
+    search_fields = ("title", "description")
+    list_filter = ("tags",)
+    
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectImage)
