964553ad7e64e4383dc73be1f574bd96ca3c65bc
Kodeck James <kodeckjames095@gmail.com>
2024-07-10 13:06:31 +0300

    second commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="projects")
+    link = models.URLField(blank=True, max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name="images", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="project_images/")
+
+    def __str__(self):
+        return f"{self.project.title} Image"
+    
+    
+
+    
+    
