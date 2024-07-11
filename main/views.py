209064d11b40de66a13bd328afb279714ec17d9c
Kodeck James <kodeckjames095@gmail.com>
2024-07-12 00:16:08 +0300

    Sixth commit

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Tag
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    return render(request, "project.html")
