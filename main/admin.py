27f8c66852ca72dd0270abf0cf0e39209f013858
Kodeck James <kodeckjames095@gmail.com>
2024-07-11 20:09:10 +0300

    Fourth Commit

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Tag, Project, ProjectImage
 # Register your models here.
+# Register your models here.
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
     extra = 1
